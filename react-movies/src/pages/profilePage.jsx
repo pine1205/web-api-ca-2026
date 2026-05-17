dda6e632b8bd02e795746b97511ccdadf2e77a74
Yuliia <20105855@setu.ie>
2026-05-17 16:22:24 +0100

    Updated profile page so it display the user name of the user that logged in

--- react-movies/src/pages/profilePage.jsx
+++ react-movies/src/pages/profilePage.jsx
@@ -1,10 +1,18 @@
+import { useContext } from "react";
 import { useNavigate } from "react-router";
+import { AuthContext } from "../contexts/authContext";
 
 const ProfilePage = () => {
+     const context = useContext(AuthContext);
     const navigate = useNavigate();
   
-    return (
-        <p>
+    return context.isAuthenticated ?  (
+          <p>
+            User profile: {context.userName}
+        </p>
+    ) : (
+     
+     <p>
             You must log in to see your profile! {" "}
             <button onClick={() => navigate('/login')}>Login</button>
       </p>
